188c6b298b7f68f18ca036731e0c05656af70746
RVirk04 <randeepvirk29@gmail.com>
2022-02-17 11:44:10 -0600

    Added the associations (belongs_to / has_many) to the Category and Product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
+  # This AR object is linked with the products table.
+
+  # A product has a many to one relationship with a category.
+  # The products table has a category_id foreign key.
+  # In other words, a product belongs to a category.
+  # All columns within products table are - product_id, name, description, price, stock_quantity, category_id, created_at, updated_at.
+  # This products table contain a column named category_id which represents a reference to category table.
+  belongs_to :category
 
   # Ensure that the product always includes a title, price and stock_quantity
   validates :title, :price, :stock_quantity, presence: true
