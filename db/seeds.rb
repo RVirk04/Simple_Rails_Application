337750281595e74f21f67e4d7fae7fb6e6c252f4
RVirk04 <randeepvirk29@gmail.com>
2022-02-17 12:26:19 -0600

    Rewrite the db/seeds.rb file such that it now pulls in product and category data from the products.csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "csv"
 require 'faker'
 
 Product.destroy_all
+Category.destroy_all
 
-#generate 637 products
-637.times do
-  Product.create([{
-    title: Faker::Commerce.product_name,
-    price: Faker::Commerce.price,
-    stock_quantity: Faker::Number.number
-  }])
+# Generate 637 products
+# 637.times do
+#   Product.create([{
+#     title: Faker::Commerce.product_name,
+#     price: Faker::Commerce.price,
+#     stock_quantity: Faker::Number.number
+#   }])
+# end
+
+# Add file path of the csv file
+csv_file = Rails.root.join('db/products.csv')
+
+csv_data = File.read(csv_file)
+products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+products.each do |product|
+  # puts product["name"]
+  category = Category.find_or_create_by(name: product["category"])
+  pro = category.products.create(
+    title: product["name"],
+    description: product["description"],
+    price: product["price"],
+    stock_quantity: product["stock quantity"]
+  )
 end
