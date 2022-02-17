e830b453955b2e6d52c401755e2705f61f6b6ba8
RVirk04 <randeepvirk29@gmail.com>
2022-02-17 00:59:18 -0600

    Display all products and updated the routes such that the Products controller's index action is trigger from a GET request to /products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
+  # get 'products/index'
+  # get 'products/show'
+  resources :products, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
