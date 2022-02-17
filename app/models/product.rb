7a06f83127e0174988553f006fd662d99020acb8
RVirk04 <randeepvirk29@gmail.com>
2022-02-17 00:23:28 -0600

    Populate the products table with 676 products using faker gem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
 
+  # Ensure that the product always includes a title, price and stock_quantity
   validates :title, :price, :stock_quantity, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Product.destroy_all
+
+#generate 637 products
+637.times do
+  Product.create([{
+    title: Faker::Commerce.product_name,
+    price: Faker::Commerce.price,
+    stock_quantity: Faker::Number.number
+  }])
+end
